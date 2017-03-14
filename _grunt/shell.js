8b89df0d6386e66218a4a5248b28b7598de17b87
Nicolas F <nicolas.funke@amplifierstrategies.com>
2017-03-13 18:02:41 -0700

    update

--- _grunt/shell.js
+++ _grunt/shell.js
@@ -9,7 +9,7 @@ module.exports = {
             command: `sed -i '' 's/TAC_PREVIEW_TOKEN/'"$TAC_PREVIEW_TOKEN"'/g' _config.yml && sed -i '' 's/TAC_CONTENTFUL_ACCESS_TOKEN/'"$TAC_CONTENTFUL_ACCESS_TOKEN"'/g' _config.yml && sed -i '' 's/TAC_CONTENTFUL_SPACE_ID/'"$TAC_CONTENTFUL_SPACE_ID"'/g' _config.yml`
     },
  	removeAPIKeys: {
-            command: `sed -i '' 's/'"$TAC_PREVIEW_TOKEN"'/TAC_PREVIEW_TOKEN/g' _config.yml && sed -i '' 's/'"$CONTENTFUL_ACCESS_TOKEN"'/CONTENTFUL_ACCESS_TOKEN/g' _config.yml && sed -i '' 's/'"$TAC_CONTENTFUL_SPACE_ID"'/TAC_CONTENTFUL_SPACE_ID/g' _config.yml`
+            command: `sed -i '' 's/'"$TAC_PREVIEW_TOKEN"'/TAC_PREVIEW_TOKEN/g' _config.yml && sed -i '' 's/'"$TAC_CONTENTFUL_ACCESS_TOKEN"'/TAC_CONTENTFUL_ACCESS_TOKEN/g' _config.yml && sed -i '' 's/'"$TAC_CONTENTFUL_SPACE_ID"'/TAC_CONTENTFUL_SPACE_ID/g' _config.yml`
     },
     commentContentful: {
             command: `sed -i '' 's/"jekyll-contentful"/#"jekyll-contentful"/g' _config.yml && sed -i '' 's/gem "jekyll-contentful"/#gem "jekyll-contentful"/g' Gemfile && sed -i '' 's/gem "jekyll-contentful-data-import"/#gem "jekyll-contentful-data-import"/g' Gemfile`
