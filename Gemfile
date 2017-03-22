98c8cb3c8458358b44ed04eacba894e29c1ced39
nicolas <nicolas.funke@amplifierstrategies.com>
2017-03-22 11:44:10 -0700

    code

--- Gemfile
+++ Gemfile
@@ -3,6 +3,6 @@ source 'https://rubygems.org'
 gem "jekyll"
 
 group :jekyll_plugins do
-  gem "jekyll-contentful"
-  gem "jekyll-contentful-data-import"
+  #gem "jekyll-contentful"
+  #gem "jekyll-contentful-data-import"
 end
---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("#accordeon h6").click(function(){
+		$("#accordeon ul ul").slideUp();
+		if(!$(this).next().is(":visible")) {
+			$(this).next().slideDown();
+		}
+	});
+	$('.menuIcon').click(function(e) {
+		e.preventDefault();
+		$('.menu-content').slideToggle();
+	});
+})
